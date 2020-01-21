13c9d412d407cf6aa73e6ec8ab328a476a5d7d87
Erik van Zijst <erik@gh.st>
2020-01-21 00:54:16 -0800

    Added moving average windowing to ADC
    The ADC readings have quite a lot jitter (~1%). This change smooths
    out the readings over a moving average.
    
    This change also introduces an offset floor level for zero luminosity
    to sidestep the issue of not being able to truly turn off the LED due
    to the ADC readings not being fixed at zero.

--- config/hpl_adc_config.h
+++ config/hpl_adc_config.h
@@ -18,7 +18,7 @@
 // <i>  Defines the bit resolution for the ADC sample values (RESSEL)
 // <id> adc_resolution
 #ifndef CONF_ADC_0_RESSEL
-#define CONF_ADC_0_RESSEL 0x2
+#define CONF_ADC_0_RESSEL 0x0
 #endif
 
 // <o> Reference Selection
