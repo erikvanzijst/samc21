b6c2ddbc49e39610be47e75f4f1a1b6834944e39
Erik van Zijst <erik.van.zijst@gmail.com>
2020-04-12 21:21:24 -0700

    Now writing to UART in real-time.
    We're now using the UART on pins 12 and 13 (RX/TX) to print the
    values read from the ADC, the period and the dampened duty cycle
    used on the PWM driver.
    
    UART runs at 115200 baud with 1 start and 1 stop bit.

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 #include <atmel_start.h>
 #include <atmel_start_pins.h>
@@ -9,6 +10,8 @@
 
 int main(void)
 {
+    struct io_descriptor *io;
+
     const uint32_t period = 10000;
     const uint32_t floor = 20;
     const float scale = (0xFFF - floor) / (float)period;
@@ -16,20 +19,30 @@ int main(void)
     uint8_t buffer[2];
     struct dwin* win = dwin_new(100);
     float val;
+    uint32_t duty;
+
+    char buf[100];
+    uint16_t buflen;
 
     /* Initializes MCU, drivers and middleware */
     atmel_start_init();
     adc_sync_enable_channel(&ADC_0, 0);
     pwm_set_parameters(&PWM_0, period, 0);
     pwm_enable(&PWM_0);
+    usart_sync_get_io_descriptor(&USART_0, &io);
+    usart_sync_enable(&USART_0);
 
     while (1) {
         adc_sync_read_channel(&ADC_0, 0, buffer, sizeof(buffer));
 
         val = (int32_t)(((buffer[1] << 8) + buffer[0]) - floor) / scale;
+        duty = (uint32_t)max(dwin_push(win, val), 0.0);
+        buflen = sprintf(buf, "ADC: %5d, Period: %ld, duty cycle: %5ld\r\n", ((buffer[1] << 8) + buffer[0]), period, duty);
 
-        pwm_set_parameters(&PWM_0, period, (uint32_t)max(dwin_push(win, val), 0.0));
+        pwm_set_parameters(&PWM_0, period, duty);
         delay_ms(1);
+        io_write(io, (uint8_t *)buf, buflen);
+
     }
     dwin_free(win);
 }
